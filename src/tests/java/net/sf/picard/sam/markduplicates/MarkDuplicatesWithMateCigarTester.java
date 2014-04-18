93e81948a4310b266894c58edd32b31787de44a3
Brad Taylor <bradt@broadinstitute.org>
2014-04-18 19:55:51 +0000

    Fixed multiple bugs in the MarkDuplicatesWithMateCigar classes and added enhancements to associated tests

--- src/tests/java/net/sf/picard/sam/markduplicates/MarkDuplicatesWithMateCigarTester.java
+++ src/tests/java/net/sf/picard/sam/markduplicates/MarkDuplicatesWithMateCigarTester.java
@@ -4,6 +4,12 @@ import net.sf.picard.cmdline.CommandLineProgram;
 
 public class MarkDuplicatesWithMateCigarTester extends AbstractMarkDuplicateFindingAlgorithmTester {
 
+    public MarkDuplicatesWithMateCigarTester(final String scoringStrategy) {
+        super();
+
+        addArg("SCORING_STRATEGY=" + scoringStrategy);
+    }
+
     @Override
     protected CommandLineProgram getProgram() { return new MarkDuplicatesWithMateCigar(); }
 }
