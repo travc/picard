8b10a10b12231196e75b540f512f89ee135c8f06
Jason Carey <jcarey@broadinstitute.org>
2014-09-23 21:01:37 -0400

    Reference fasta should be optional.

--- src/java/picard/cmdline/CommandLineProgram.java
+++ src/java/picard/cmdline/CommandLineProgram.java
@@ -97,7 +97,7 @@ public abstract class CommandLineProgram {
     @Option(doc="Whether to create an MD5 digest for any BAM or FASTQ files created.  ", common=true)
     public boolean CREATE_MD5_FILE = Defaults.CREATE_MD5;
 
-    @Option(doc = "Reference fasta file.", common = true)
+    @Option(doc = "Reference fasta file.", common = true, optional = true)
     public File REFERENCE_FASTA = Defaults.REFERENCE_FASTA;
 
     private final String standardUsagePreamble = CommandLineParser.getStandardUsagePreamble(getClass());
